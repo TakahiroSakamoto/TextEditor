0085301351eb8fec613c957c5387999f47565c54
坂本貴宏 <ts.1103.a.a@gmail.com>
2018-06-27 13:42:32 +0900

    Viewの作成は別クラスで行う

---
+++ TestTextView/View/UrlButton.swift
@@ -0,0 +1,21 @@
+//
+//  UrlButton.swift
+//  TestTextView
+//
+//  Created by 坂本貴宏 on 2018/06/26.
+//  Copyright © 2018年 坂本貴宏. All rights reserved.
+//
+
+import UIKit
+
+class UrlButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
